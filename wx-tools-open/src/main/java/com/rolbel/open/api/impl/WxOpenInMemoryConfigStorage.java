730a560aa16aa995eca9e6e4444a742946c2a91e
liuy <820310472@qq.com>
2018-09-29 17:16:23 +0800

    更新项目

--- wx-tools-open/src/main/java/com/rolbel/open/api/impl/WxOpenInMemoryConfigStorage.java
+++ wx-tools-open/src/main/java/com/rolbel/open/api/impl/WxOpenInMemoryConfigStorage.java
@@ -18,8 +18,6 @@ import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * 基于内存的微信配置provider，在实际生产环境中应该将这些配置持久化
- *
- * @author <a href="https://github.com/007gzs">007</a>
  */
 public class WxOpenInMemoryConfigStorage implements WxOpenConfigStorage {
     private String componentAppId;
