a29adbca77d408e0445ed653bd5c4a122e4724e9
liuy <liuy@hgobox>
2018-12-27 15:35:02 +0800

    更新

--- wx-tools-ma/src/main/java/com/rolbel/ma/lock/RedisLock.java
+++ wx-tools-ma/src/main/java/com/rolbel/ma/lock/RedisLock.java
@@ -3,7 +3,6 @@ package com.rolbel.ma.lock;
 import redis.clients.jedis.Jedis;
 import redis.clients.util.Pool;
 
-import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
@@ -12,7 +11,6 @@ public class RedisLock implements Lock, java.io.Serializable {
     private static final String LOCK_SUCCESS = "OK";
     private static final String SET_IF_NOT_EXIST = "NX";
     private static final String SET_WITH_EXPIRE_TIME = "PX";
-    private static final Long RELEASE_SUCCESS = 1L;
 
     private final Pool<Jedis> jedisPool;
     private final String key;
@@ -50,12 +48,7 @@ public class RedisLock implements Lock, java.io.Serializable {
     @Override
     public void unlock() {
         try (Jedis jedis = this.jedisPool.getResource()) {
-            String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
-            Object result = jedis.eval(script, Collections.singletonList("wx_ma_distribute_lock:" + this.key), Collections.singletonList(this.key));
-
-            if (RELEASE_SUCCESS.equals(result)) {
-                return;
-            }
+            jedis.del("wx_ma_distribute_lock:" + this.key);
         }
     }
 
--- wx-tools-mp/src/main/java/com/rolbel/mp/lock/RedisLock.java
+++ wx-tools-mp/src/main/java/com/rolbel/mp/lock/RedisLock.java
@@ -3,7 +3,7 @@ package com.rolbel.mp.lock;
 import redis.clients.jedis.Jedis;
 import redis.clients.util.Pool;
 
-import java.util.Collections;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
@@ -12,7 +12,6 @@ public class RedisLock implements Lock, java.io.Serializable {
     private static final String LOCK_SUCCESS = "OK";
     private static final String SET_IF_NOT_EXIST = "NX";
     private static final String SET_WITH_EXPIRE_TIME = "PX";
-    private static final Long RELEASE_SUCCESS = 1L;
 
     private final Pool<Jedis> jedisPool;
     private final String key;
@@ -25,7 +24,7 @@ public class RedisLock implements Lock, java.io.Serializable {
     @Override
     public void lock() {
         try (Jedis jedis = this.jedisPool.getResource()) {
-            String result = jedis.set("wx_mp_distribute_lock:" + this.key, this.key, SET_IF_NOT_EXIST, SET_WITH_EXPIRE_TIME, 30);
+            String result = jedis.set("wx_mp_distribute_lock:" + this.key, UUID.randomUUID().toString(), SET_IF_NOT_EXIST, SET_WITH_EXPIRE_TIME, 30);
             if (!LOCK_SUCCESS.equals(result)) {
                 this.lock();
             }
@@ -50,12 +49,7 @@ public class RedisLock implements Lock, java.io.Serializable {
     @Override
     public void unlock() {
         try (Jedis jedis = this.jedisPool.getResource()) {
-            String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
-            Object result = jedis.eval(script, Collections.singletonList("wx_mp_distribute_lock:" + this.key), Collections.singletonList(this.key));
-
-            if (RELEASE_SUCCESS.equals(result)) {
-                return;
-            }
+            jedis.del("wx_mp_distribute_lock:" + this.key);
         }
     }
 
